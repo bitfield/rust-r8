d58d13f51b6d89c0b243a099471029b510e7cde0
John Arundel <john@bitfieldconsulting.com>
2026-08-24 14:20:49 +0100

    tweaks

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub const NOP: u8 = 1;
+
 #[derive(Debug)]
 pub struct Cpu {
     pub pc: u16,
@@ -13,6 +15,12 @@ impl Default for Cpu {
     }
 }
 
+impl Cpu {
+    pub fn step(&mut self) {
+        // Over to you to implement `step`!
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,4 +31,13 @@ mod tests {
         assert_eq!(cpu.pc, 0, "wrong initial PC");
         assert_eq!(*cpu.mem.first().unwrap(), 0, "wrong memory contents");
     }
+
+    // Uncomment this test once the previous test passes!
+    // #[test]
+    // fn nop_instruction_increments_pc() {
+    //     let mut cpu = Cpu::default();
+    //     cpu.mem[0] = NOP;
+    //     cpu.step();
+    //     assert_eq!(cpu.pc, 1, "wrong PC after step()")
+    // }
 }
